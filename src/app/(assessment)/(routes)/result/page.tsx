b7499b9c67c19cb385d04563f65bcc9a4f8e46d8
DKVGITHUB <dolavalentin12@gmail.com>
2024-10-07 18:14:26 +0000

    Update page.tsx

--- src/app/(assessment)/(routes)/result/page.tsx
+++ src/app/(assessment)/(routes)/result/page.tsx
@@ -5,10 +5,22 @@ import Career from "./_components/career";
 import College from "./_components/college";
 import Major from "./_components/major";
 import Program from "./_components/program";
+import { Button } from "@/components/ui/button";
 
 export default function Result() {
+  const handlePrint = () => {
+    window.print();
+  };
   return (
     <div className="max-w-[1380px] w-full my-0 px-5 py-0">
+      <div className="p-5">
+        <Button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handlePrint}
+        >
+          Print Result
+        </Button>
+      </div>
       <div>
         {/* Header with a dynamic title */}
         <div className="bg-[#00aeef] border-[#00aeef]  px-[15px] py-2.5 border-b-transparent border-b border-solid">
